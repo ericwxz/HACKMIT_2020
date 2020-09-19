de9f14492609226a0aeeafbf137956384e191784
alexisteh <wenhui.teh23@gmail.com>
2020-09-19 13:58:52 -0400

    hoohoo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function(){
 
 console.log('hello')
 
-
+if (sessionStorage.getItem('userkey') != null && sessionStorage.getItem('username') != null){
+    coverUpLogin("LoggedInAt " + sessionStorage.getItem('userkey') + " " + sessionStorage.getItem('username'))
+}
 
 
 
