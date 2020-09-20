a9d5278c1efa5c0f6b7658c0fb6aa259bc3e0c1e
Mudi Yang <mudi.yang@yale.edu>
2020-09-19 23:27:44 -0400

    added methods for adding fufillers and completing requests

--- Backend/data.py
+++ Backend/data.py
@@ -6,7 +6,7 @@ import pickle
 #use userID and requestID to index the dataframes
 #pandas dataframes for storing information about users. 
 class All_Users:
-	columns = ['username', 'location', 'points', 'contact_info']
+	columns = ['username', 'location', 'points', 'contact_info', 'requests', 'fufillments']
 	Users = pd.DataFrame(columns = columns) #dataframe storing user information
 
 	#hash table of usernames and passwords. use check_user(username, password) to see if user exists and if the user exists, if the correct password was given
@@ -30,7 +30,7 @@ class All_Users:
 		#check if user is already in the databse
 		if self.check_user(user[0], password) == 0: #not in database, add to database
 			self.Users_hash[user[0]] = password
-			self.Users.loc[userID] = user
+			self.Users.loc[userID] = user + [[],[]]
 			return 1
 		
 		else: #in database. do nothing
@@ -63,24 +63,38 @@ class All_Users:
 
 #pandas dataframes for storing infomation about requests
 class All_Requests:
-	columns = ['requester', 'accesser', 'time', 'location', 'status', 'description']
+	columns = ['requester', 'fufiller', 'time', 'location', 'status', 'description']
 	Requests = pd.DataFrame(columns = columns)
 
 	#list of completed tasks, used for the sucess stories page
 	queue = []
 
 
-	def add_request(self, request, requestID):
-		"add request data to the Request dataframe. request is a list with the following format:['requester (ID)', 'accesser (ID)', 'time', 'location', 'status', 'description'] and requestID is the unique identifier for the request. return 1 on success"
+	def add_request(self, request, requestID, All_Users):
+		"add request data to the Request dataframe. request is a list with the following format:['requester (ID)', 'fufiller (ID)', 'time', 'location', 'status', 'description'] and requestID is the unique identifier for the request. return 1 on success. All_Users refers to the instance of the All_Users class. status will be set to -1, fufiller will be set to empty string"
+		request[1] = "" #making the fufiller null since it has not been fufilled yet
+		request[4] = -1 #marking the status as unfufilled
 		self.Requests.loc[requestID] = request
+		
+		#add this requestID to the list of requests made 
+		requesterID = request[0]
+		All_Users.Users.loc[requesterID]['requests'].append(requestID)
+
 		self.Requests.sort_values(by = ['time'], ascending=False, inplace=True)
+		return 1
+
+	def add_fufiller(self, requestID, fufillerID, All_Users):
+		"add a fufiller to a requestID. the status will be set to 0. All_Users refers to the instance of the All_Users class. return 1 on success"
+		self.Requests.loc[requestID]['fufiller'] = fufillerID
+		self.Requests.loc[requestID]['status'] = 0
+		All_Users.Users.loc[fufillerID]['fufillments'].append(requestID)
 		return 1
 
 
 	def completed_request(self, requestID, All_Users):
-		"set reqestID to completed and increment the points for the accessor in the request. Also updates the queue. return 1 on success"
+		"set reqestID to completed and increment the points for the accessor in the request. Also updates the queue. return 1 on success. All_users refers to the instance of the All_users class, status will be set as 1"
 		self.Requests.loc[requestID]['status'] = 1
-		All_Users.add_point(self.Requests.loc[requestID]['accesser'])
+		All_Users.add_point(self.Requests.loc[requestID]['fufiller'])
 		
 		
 		string = self.Requests.loc[requestID]['description'] +": " + self.Requests.loc[requestID]['location']+ ", " + str(self.Requests.loc[requestID]['time'])
